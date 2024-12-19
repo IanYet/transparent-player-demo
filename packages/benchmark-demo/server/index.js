9f28f7de5d20d3cd545e70d463406151b73152c2
IanYet <yt2015.97@gmail.com>
2024-12-19 18:22:41 +0800

    delete output

--- packages/benchmark-demo/server/index.js
+++
@@ -1,3 +0,0 @@
-import { main as nodeCanvas } from './node-canvas/index';
-nodeCanvas();
-//# sourceMappingURL=index.js.map
--- packages/benchmark-demo/server/node-canvas/draw.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function draw(): Promise<void>;
-//# sourceMappingURL=draw.d.ts.map
